9003608f98c7602c224b36fc1ab55f210d0ed838
Ahmet GONEN <gonen.ahmet62@gmail.com>
2026-03-06 12:08:05 +0000

    Real change in feature branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,1 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, Lab 6!");
-    }
-}
+public class Hello { public static void main(String[] args) { System.out.println("Hello from Feature"); } }
